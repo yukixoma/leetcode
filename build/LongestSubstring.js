62ac618467db994e9b9e9b95c4d095aba3d7d56d
yukixoma08@gmail.com <yukixoma08@gmail.com>
2025-07-29 11:47:55 +0900

    Init

---
+++ build/LongestSubstring.js
@@ -0,0 +1,16 @@
+function lengthOfLongestSubstring(s) {
+    let max = 0;
+    let subString = "";
+    for (let i = 0; i < s.length; i++) {
+        let index = subString.indexOf(s[i]);
+        if (index === -1) {
+            subString += s[i];
+        }
+        else {
+            max = subString.length > max ? subString.length : max;
+            subString = subString.slice(index + 1) + s[i];
+        }
+    }
+    return subString.length > max ? subString.length : max;
+}
+console.log(lengthOfLongestSubstring("pwwkew"));
---
+++ build/TwoSum.js
@@ -0,0 +1,9 @@
+function twoSum(nums, target) {
+    for (let firstIndex = 0; firstIndex < nums.length; firstIndex++) {
+        let secondIndex = nums.indexOf(target - nums[firstIndex], firstIndex + 1);
+        if (secondIndex != -1) {
+            return [firstIndex, secondIndex];
+        }
+    }
+}
+console.log(twoSum([2, 7, 11, 15], 9));
